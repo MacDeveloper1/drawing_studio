914527837fbfa80c0e1b4126b9c7918069fb3b69
Aleksei Timkov <at@mactwin.eu>
2024-04-10 09:44:28 +0200

    Add instruction

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,20 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(useMaterial3: false),
-      home: const Scaffold(
-        body: Center(
-          child: ZoneFloorPlan(),
+      home: Scaffold(
+        backgroundColor: Colors.grey,
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          alignment: Alignment.center,
+          child: const Column(
+            children: [
+              Text('To draw shape:\n'
+                  '1: Tap on image\n'
+                  '2: To complete shape do double click'),
+              SizedBox(height: 20),
+              Expanded(child: ZoneFloorPlan()),
+            ],
+          ),
         ),
       ),
     );
